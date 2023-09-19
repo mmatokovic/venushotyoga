f2a4144c1418ea09c67e1eaeae27d1c6d8fb77cd
mmatokovic <mislav.matokovic@crosig.hr>
2023-09-19 13:05:41 +0200

    Test

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -29,11 +29,55 @@ document.addEventListener("DOMContentLoaded", () => {
                 popup.classList.add('hidden');
             });
         }
-
+        
         window.addEventListener('click', (event) => {
             if (event.target === popup) {
                 popup.classList.add('hidden');
             }
         });
     }
+});
+
+const contactForm = document.getElementById('contact') as HTMLFormElement;
+const contactSubmitButton = contactForm.querySelector('button[type="submit"]') as HTMLButtonElement;
+
+// Add an event listener to the form submission
+contactForm.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Update the submit button text to "Sending..."
+  contactSubmitButton.textContent = 'Sending...';
+  contactSubmitButton.style.backgroundColor = 'green';
+
+  // Simulate a delay to mimic server-side processing
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // Clear all form fields
+  contactForm.reset();
+
+  // Update the submit button text to "Sent"
+  contactSubmitButton.textContent = 'Sent';
+  contactSubmitButton.style.backgroundColor = '';
+});
+
+const form = document.getElementById('newsletter') as HTMLFormElement;
+const submitButton = form.querySelector('button[type="submit"]') as HTMLButtonElement;
+
+// Add an event listener to the form submission
+form.addEventListener('submit', async (event) => {
+  event.preventDefault(); // Prevent the form from submitting normally
+
+  // Update the submit button text to "Sending..."
+  submitButton.textContent = 'Sending...';
+  submitButton.style.backgroundColor = 'green';
+
+  // Simulate a delay to mimic server-side processing
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  // Clear all form fields
+  form.reset();
+
+  // Update the submit button text to "Sent"
+  submitButton.textContent = 'Sent';
+  submitButton.style.backgroundColor = '';
 });
