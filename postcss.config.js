72e12803310bd7d77b9e0a827da519d66f55e54a
mmatokovic <27929908+mmatokovic@users.noreply.github.com>
2025-11-12 20:11:42 +0100

    [develop] refactor and bum versions

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.config.js';
-const tailwind = require('tailwindcss')(tailwindConfig);
-const autoprefixer = require('autoprefixer');
-
-module.exports = {
-	plugins: [ tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
-};
